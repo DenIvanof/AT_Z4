ca3655c151df6b89e1a84c53292202e298a2e19e
Ivanich <deniska090798@gmail.com>
2024-03-25 00:44:25 +0400

    final

--- src/test/java/ru/netology/OrderCardTest.java
+++ src/test/java/ru/netology/OrderCardTest.java
@@ -40,6 +40,54 @@ public class OrderCardTest {
         //Thread.sleep(500);
 
     }
+    //Необязательная часть
+    @Test
+    void searchCityTest() throws InterruptedException {
+        open("http://localhost:9999/");
+        $("[data-test-id='city'] input").setValue("Са");
+        //Thread.sleep(1000);
+        $(byText("Самара")).click();
+        String planningDate = generateData(365, "dd.MM.yyyy");
+        $("[data-test-id='date'] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.DELETE);
+        $("[data-test-id='date'] input").setValue(planningDate);
+        //Thread.sleep(1000);
+        $("[data-test-id='name'] input").setValue("Иванов Денис");
+        //Thread.sleep(500);
+        $("[data-test-id='phone'] input").setValue("+71112223344");
+        //Thread.sleep(500);
+        $("[data-test-id='agreement']").click();
+        //$(".checkbox__box").click();
+        //Thread.sleep(500);
+        $("button.button").click();
+        $(".notification__content")
+                .shouldBe(Condition.visible, Duration.ofSeconds(15))
+                .shouldHave(Condition.exactText("Встреча успешно забронирована на " + planningDate));
+        //Thread.sleep(500);
+
+    }
+
+    @Test
+    void selectionCalendarTest() throws InterruptedException {
+        open("http://localhost:9999/");
+        $("[data-test-id='city'] input").setValue("Са");
+        //Thread.sleep(1000);
+        $(byText("Самара")).click();
+        $("button").click();
+        Thread.sleep(1000);
+        $("[data-test-id='name'] input").setValue("Иванов Денис");
+        //Thread.sleep(500);
+        $("[data-test-id='phone'] input").setValue("+71112223344");
+        //Thread.sleep(500);
+        $("[data-test-id='agreement']").click();
+        //$(".checkbox__box").click();
+        //Thread.sleep(500);
+        $(".button.button").click();
+        //$(".notification__content")
+        // .shouldBe(Condition.visible, Duration.ofSeconds(15))
+        // .shouldHave(Condition.exactText("Встреча успешно забронирована на " + ));
+        //Thread.sleep(500);
+
+    }
 
 
 
